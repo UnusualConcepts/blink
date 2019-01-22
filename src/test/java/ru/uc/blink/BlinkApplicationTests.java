29ffdf438edb17b201a2d782367df2b7504860b0
Sergey Lobin <ifitwasi@gmail.com>
2019-01-22 17:58:56 +0300

    We are starting new project!

---
+++ src/test/java/ru/uc/blink/BlinkApplicationTests.java
@@ -0,0 +1,17 @@
+package ru.uc.blink;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlinkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
