8a97c3419d93de81985d40b39719f3f32ff73d03
Sergey Lobin <sergey.lobin@gmail.com>
2019-02-27 11:18:55 +0300

    run echo on ci

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ./gradlew build docker
-#docker login --username=2heoh --password=blink1234
-#docker tag uc/blink 2heoh/blink
-#docker push 2heoh/blink
+docker login --username=2heoh --password=blink1234
+docker tag uc/blink 2heoh/blink
+docker push 2heoh/blink
