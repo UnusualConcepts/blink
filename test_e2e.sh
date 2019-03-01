c56c067805cc99a18beb17645b679a10db717611
Sergey Lobin <sergey.lobin@gmail.com>
2019-03-01 21:44:41 +0300

    run e2e tests (should be broken)ˆ

--- test_e2e.sh
+++ test_e2e.sh
@@ -2,10 +2,10 @@
 
 if [ "$1" = "local" ]; then
     docker run -d -p 8088:8080 --name e2e_test -t 2heoh/blink:master
-    ./gradlew e2e
+    ./gradlew test :e2e
     docker rm $(docker stop $(docker ps -f name=e2e_test --format {{.ID}}))
 else
-    sudo gradle e2e
+    sudo gradle test :e2e
 fi
 
 
